fb9a3850869cd8dba415629e5c065a0a4ee36107
Marcello Vieira Mueno <deoliveiral767@gmail.com>
2022-12-12 17:23:07 -0300

    first commit

---
+++ src/main/java/br/com/jdev/jdevbackend/JdevbackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.jdev.jdevbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdevbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdevbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jdev/jdevbackend/JdevbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jdev.jdevbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdevbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
