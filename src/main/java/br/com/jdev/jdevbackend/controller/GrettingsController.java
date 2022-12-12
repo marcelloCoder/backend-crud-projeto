f7f74c9918af23d82d4a6b7836d1e1af4b977e9b
Marcello Vieira Mueno <deoliveiral767@gmail.com>
2022-12-12 17:40:17 -0300

    commit

---
+++ src/main/java/br/com/jdev/jdevbackend/controller/GrettingsController.java
@@ -0,0 +1,19 @@
+package br.com.jdev.jdevbackend.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GrettingsController {
+	
+	@RequestMapping(value = "/{/name}", method = RequestMethod.GET)
+	@ResponseStatus(HttpStatus.OK)
+	public String greetingText(@PathVariable String name) {
+		return "Hello " + name + "!";
+	}
+
+}
